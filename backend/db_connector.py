54134862bd44e358dc2c4b96e20fc0fe5056c0e4
Justin <73923621+truthinthorns@users.noreply.github.com>
2025-10-03 21:22:30 -0400

    Update db_connector.py

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -7,6 +7,6 @@ from backend.models.todo import Todo
 
 async def init_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb+srv://user:2jMhJsFvIAfQcMn2@kanboard.aw9zrws.mongodb.net/"
+        
     )
     await init_beanie(database=client["db"], document_models=[MongoUser, Todo])
