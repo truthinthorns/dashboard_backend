229dbd3aeeba2cfc2d1a6e6b9a43c304c138b12e
Justin <justwells111@gmail.com>
2026-05-14 15:26:40 -0400

    add some changes for dockerization

--- backend/db_connector.py
+++ backend/db_connector.py
@@ -7,6 +7,6 @@ from backend.models.todo import Todo
 
 async def init_db():
     client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://user:password@localhost:27017"
+        "mongodb://user:password@mongodb:27017"
     )
     await init_beanie(database=client["db"], document_models=[MongoUser, Todo])
